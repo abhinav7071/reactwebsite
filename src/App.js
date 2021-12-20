d34b1509f43b594f142571c0090d9abdb438a20b
Abhinav Srivastava <abhinavsrivastava@virtualemployee.com>
2021-12-20 16:54:13 +0530

    react website git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from 'axios';
 
 function App() {
+
+  //const[search,setSearch] = useState("chicken");
+  const[search,setSearch] = useState("");
+  const[recipes,setRecipes] = useState([]);//by feault set kiya ki kis type ka data isme ayega
+
+  /****Get API All Recipes****/
+  const APP_ID = "387fdcf9";
+  const APP_KEY = "f2e8245e76b2eedf03ff9d79d1915e16";
+
+  //Featch Data from API on load
+    useEffect(async()=>{
+      getRecipes();   
+    },[])
+
+  //Get Recpies from API
+  const getRecipes = async() =>{
+    const result = await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    setRecipes(result.data.hits);//data set karr diya jo API se aya 
+  }
+
+  //Search item on button click,ye search ki value daal kk getrceipe se API hit karke data laa k display kara rha
+  const onSearchClick = () => {
+    getRecipes();
+  }
+
+  /****Input Chnage On Text*****/
+  const onInputChange = e => {
+    setSearch(e.target.value);
+    console.log(e.target.value);  
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header search={search} onInputChange={onInputChange} onSearchClick={onSearchClick}  />
+        <Recipes recipes={recipes} />
     </div>
   );
 }
---
+++ src/components/Recipes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RecipeItem from './RecipeItem';
+
+const Recipes = (props) =>
+{
+  const {recipes} = props;
+  return(
+      <div className="row">
+        {recipes.map(recipe => <RecipeItem name={recipe.recipe.label} 
+                                           image={recipe.recipe.image} 
+                                          ingredientLines={recipe.recipe.ingredientLines} />)}
+      </div>
+  )
+}
+
+export default Recipes;
